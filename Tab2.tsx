2893c53c721267674696886682c6bce1dcba4461
kcassin2 <31949074+kcassin2@users.noreply.github.com>
2020-03-26 13:54:59 -0400

    Add files via upload

---
+++ Tab2.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, 
+         IonFab, IonFabButton, IonIcon, IonGrid, IonRow, 
+         IonCol, IonImg, IonActionSheet } from '@ionic/react';
+
+const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Gallery</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+ 
+
+      <IonContent>
+
+
+
+      
+  <IonGrid>
+    <IonRow>
+      {photos.map((photo, index) => (
+        <IonCol size="6" key={index}>
+          <IonImg src={photo.base64 ?? photo.webviewPath} />
+        </IonCol>
+      ))}
+    </IonRow>
+  </IonGrid>
+      <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Tab 2</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 2 page" />
+        
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
